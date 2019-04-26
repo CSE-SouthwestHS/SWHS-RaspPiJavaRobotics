f6f49947d6e47edfab54a1a07decd722fba80f2e
Seay Zagar <seaystir@gmail.com>
2019-04-26 09:51:10 -0500

    Simplifying codes and variable name changes for XY transmitting

--- WebsocketTransmitter_v.1.2/app.js
+++ WebsocketTransmitter_v.1.2/app.js
@@ -2,66 +2,48 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app)
 var url = require('url')
 var fs = require('fs')
-
-
 //This will open a server at localhost:5000. Navigate to this in your browser.
 app.listen(5000);
-
 // Http handler function
 function handler (req, res) {
-
-    // Using URL to parse the requested URL
-    var path = url.parse(req.url).pathname;
-
-    // Managing the root route
-    if (path == '/') {
-        index = fs.readFile(__dirname+'/public/index.html',
-            function(error,data) {
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load index.html");
-                }
-                res.writeHead(200,{'Content-Type': 'text/html'});
-                res.end(data);
-            });
-    // Managing the route for the javascript files
-    } else if( /\.(js)$/.test(path) ) {
-        index = fs.readFile(__dirname+'/public'+path,
-            function(error,data) {
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load " + path);
-                }
-                res.writeHead(200,{'Content-Type': 'text/plain'});
-                res.end(data);
-            });
-    // Managing the route for the css files
-    } else if( /\.(css)$/.test(path) ) {
-        index = fs.readFile(__dirname+'/public'+path,
-            function(error,data) {
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load " + path);
-                }
-                res.writeHead(200,{'Content-Type': 'text/plain'});
-                res.end(data);
-            });
-    } else {
-        res.writeHead(404);
-        res.end("Error: 404 - File not found.");
-    }
-
+  // Using URL to parse the requested URL
+  var path = url.parse(req.url).pathname;
+  // Managing the root route
+  if (path == '/') {
+    index = fs.readFile(__dirname+'/public/index.html',
+    function(error,data) {
+      if (error) {
+        res.writeHead(500);
+        return res.end("Error: unable to load index.html");
+      }
+      res.writeHead(200,{'Content-Type': 'text/html'});
+      res.end(data);
+    });
+    // Managing the route for the javascript or css files
+  } else if( /\.(js)$/.test(path) || /\.(css)$/.test(path) ) {
+    index = fs.readFile(__dirname+'/public'+path,
+      function(error,data) {
+        if (error) {
+          res.writeHead(500);
+          return res.end("Error: unable to load " + path);
+        }
+        res.writeHead(200,{'Content-Type': 'text/plain'});
+        res.end(data);
+      });
+  } else {
+    res.writeHead(404);
+    res.end("Error: 404 - File not found.");
+  }
 }
-
 // Web Socket Connection
 io.sockets.on('connection', function (socket) {
   // If we recieved a command from a client
   socket.on('connection', function(data){
-	  dataXY = data["xyCoordinates"];
-	  console.log(dataXY);
+	  transmitterData = data["transmitterData"];
+	  console.log(transmitterData);
     // Set a timer for when we should stop
     setTimeout(function(){
-        socket.emit('connection');
+      socket.emit('connection');
     }, 0);
   });
 });
