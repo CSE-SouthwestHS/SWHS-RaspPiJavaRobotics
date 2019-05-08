dff7f6d21d19c9c980ded17e9b9b95df70702377
williammunnich <williammunnich@gmail.com>
2019-05-08 08:37:37 -0700

    updated help

--- push.js
+++ push.js
@@ -1,7 +1,9 @@
 //src="/socket.io/socket.io.js"
 var WebSocket = require('ws');
-const connection = new WebSocket('ws://localhost:1341');
+const connection = new WebSocket('ws://localhost:5000');
 
 connection.onopen = function () {
-  
+  connection.emit('generic');
+  connection.send('generic');
+  connection.send('generic');
 };
