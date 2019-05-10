f0030dee8e48639afe80fed12d50e1e43f91b4c1
williammunnich <williammunnich@gmail.com>
2019-05-10 10:04:34 -0700

    small change

--- push.js
+++ push.js
@@ -1,8 +1,14 @@
 //this is not the full front end, just a part of what we hope to implement for communication
-var WebSocket = require('ws');
-const connection = new WebSocket('ws://localhost:5000');
-connection.onopen = function () {
-  connection.emit('generic');
-  connection.send('generic');
-  connection.send('generic');
-};
+
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+
+
+xmlHttp.open("GET", localhost, true); // true for asynchronous
+xmlHttp.send(null);
